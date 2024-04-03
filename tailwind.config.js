cd16a95ec0c0c7e6a1c4883571e49d341c736ff0
Hæmïm Shêïkh <hamimboss66@gmail.com>
2024-04-03 21:42:38 +0600

    friend project commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        screens:{
+            sm:"480"
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
